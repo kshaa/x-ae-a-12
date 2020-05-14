cb06510fec95f680737d743036d574730f6d92da
Krisjanis Veinbahs <krisjanis.veinbahs@gmail.com>
2020-05-14 17:40:42 +0300

    Add more tables for push notification messaging

--- app/models/user_models.py
+++ app/models/user_models.py
@@ -30,6 +30,27 @@ class User(db.Model, UserMixin):
     roles = db.relationship('Role', secondary='users_roles',
                             backref=db.backref('users', lazy='dynamic'))
 
+# Define the API Keys data model
+class APIKeys(db.Model):
+    __tablename__ = 'apikeys'
+    id = db.Column(db.Integer(), primary_key=True)
+    owner_user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    key = db.Column(db.String(255), nullable=False, server_default=u'', unique=True)
+
+# Define the Topic data model
+class Topic(db.Model):
+    __tablename__ = 'topics'
+    id = db.Column(db.Integer(), primary_key=True)
+    owner_user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    name = db.Column(db.String(50), nullable=False, server_default=u'', unique=True)  # for @roles_accepted()
+    label = db.Column(db.Unicode(255), server_default=u'') # for display purposes
+
+# Define the Messages data model
+class Messages(db.Model):
+    __tablename__ = 'message'
+    id = db.Column(db.Integer(), primary_key=True)
+    topic_id = db.Column(db.Integer(), db.ForeignKey('topics.id', ondelete='CASCADE'))
+    content = db.Column(db.Unicode(255), server_default=u'') # for display purposes
 
 # Define the Role data model
 class Role(db.Model):
@@ -37,7 +58,6 @@ class Role(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(50), nullable=False, server_default=u'', unique=True)  # for @roles_accepted()
     label = db.Column(db.Unicode(255), server_default=u'')  # for display purposes
-
 
 # Define the UserRoles association model
 class UsersRoles(db.Model):
