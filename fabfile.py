53a0d08b2876ad43dda156d02758a7c3b56e5117
Krisjanis Veinbahs <krisjanis.veinbahs@gmail.com>
2020-05-15 16:35:11 +0300

    Fix up messy old code

--- fabfile.py
+++
@@ -1,8 +0,0 @@
-# Definition file for Fabric -- an SSH command-line tool
-
-from fabric.operations import local
-from fabric.api import task
-
-@task
-def runserver():
-    local('python manage.py runserver')
--- migrations/versions/0001c8ac1a69_initial_version.py
+++ migrations/versions/0001c8ac1a69_initial_version.py
@@ -25,17 +25,15 @@ def upgrade():
     )
     op.create_table('users',
     sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('username', sa.String(length=50), nullable=False),
     sa.Column('password', sa.String(length=255), server_default='', nullable=False),
     sa.Column('reset_password_token', sa.String(length=100), server_default='', nullable=False),
     sa.Column('email', sa.String(length=255), nullable=False),
-    sa.Column('confirmed_at', sa.DateTime(), nullable=True),
+    sa.Column('email_confirmed_at', sa.DateTime(), nullable=True),
     sa.Column('is_active', sa.Boolean(), server_default='0', nullable=False),
     sa.Column('first_name', sa.String(length=50), server_default='', nullable=False),
     sa.Column('last_name', sa.String(length=50), server_default='', nullable=False),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email'),
-    sa.UniqueConstraint('username')
     )
     op.create_table('users_roles',
     sa.Column('id', sa.Integer(), nullable=False),
