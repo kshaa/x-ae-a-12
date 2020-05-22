9c49c61a03f03a705ba108ab1a7ca7e48f4540ed
Krisjanis Veinbahs <krisjanis.veinbahs@gmail.com>
2020-05-22 16:13:00 +0300

    Polish notify script

--- notify.sh
+++ notify.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-TOPIC_CODE=$1
-MESSAGE=$2
-API_URL="http://localhost:5000/api/notify"
+# export API_KEY="<LONG_API_KEY_HERE>"
+# export HOST="http://localhost:5000"
+TOPIC_CODE="$1"
+MESSAGE="$2"
+API_URL="${HOST%/}/api/notify"
 
-if [ -z $API_KEY ]; then echo "API_KEY environment variable not defined" && exit 1; fi
-if [ -z $TOPIC_CODE ]; then echo "TOPIC_CODE (first argument) not defined" && exit 1; fi
-if [ -z $MESSAGE ]; then echo "MESSAGE (second argument) not defined" && exit 1; fi
+if [ -z "$API_KEY" ]; then echo "API_KEY environment variable not defined" && exit 1; fi
+if [ -z "$HOST" ]; then echo "HOST environment variable not defined" && exit 1; fi
+if [ -z "$TOPIC_CODE" ]; then echo "TOPIC_CODE (first argument) not defined" && exit 1; fi
+if [ -z "$MESSAGE" ]; then echo "MESSAGE (second argument) not defined" && exit 1; fi
 
-echo "Sending to topic '$TOPIC_CODE' message '$MESSAGE'"
-curl -X GET "$API_URL?api_key=$API_KEY&topic_code=$TOPIC_CODE&message=$MESSAGE"
+echo "Sending to API '$API_URL' topic '$TOPIC_CODE' message '$MESSAGE'"
+curl -G "$API_URL" \
+    --data-urlencode "api_key=$API_KEY" \
+    --data-urlencode "topic_code=$TOPIC_CODE" \
+    --data-urlencode "message=$MESSAGE"
